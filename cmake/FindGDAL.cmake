bda396ffe154019c9cbda9dd8850b0b9640ebd8c
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-22 10:17:52 -0500

    initial

--- cmake/FindGDAL.cmake
+++ cmake/FindGDAL.cmake
@@ -1,10 +1,16 @@
-# Minimal FindGDAL.cmake for common setups
-find_path(GDAL_INCLUDE_DIR gdal.h
-  PATHS /usr/include /usr/local/include
+find_path(GDAL_INCLUDE_DIR
+  NAMES gdal.h gdal/gdal.h
+  PATHS
+    /usr/include
+    /usr/include/gdal
+    /usr/local/include
+    /usr/local/include/gdal
+  PATH_SUFFIXES gdal
 )
 
-find_library(GDAL_LIBRARY NAMES gdal
-  PATHS /usr/lib /usr/local/lib
+find_library(GDAL_LIBRARY
+  NAMES gdal
+  PATHS /usr/lib /usr/local/lib /usr/lib/x86_64-linux-gnu
 )
 
 include(FindPackageHandleStandardArgs)
